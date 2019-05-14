e410bb653e4af069fc42de15d88c9ee6846118a7
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-05-14 13:12:53 +0530

    Create Breaking Hello World.go

---
+++ Notes/Breaking Hello World.go
@@ -0,0 +1,20 @@
+//we are going to break our 1st go program .....today...
+
+
+package main
+//package main-- this is called the package declaration..
+
+import "fmt"
+//import --> this imports so that how we include codes from other packages..
+//fmt-->
+//(it is shorthand for format)
+// format implements formatting for input and output
+//"in between this , is called a string literal(in double quotes")
+
+func main(){
+//func--> it is used to declare a function.
+	fmt.Println("Hello World")
+}
+
+//Thanx for Joining
+
