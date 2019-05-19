823f575730e7a2e3d0287279a6fd3aad4622c8b2
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-05-19 12:52:16 +0530

    Update constants.go

--- Programs/constants.go
+++ Programs/constants.go
@@ -1,7 +1,10 @@
+//We are writing a program to assign a constant here.
+
 package main
 import "fmt"
 func main(){
-	const x  string = "Hello ,Bro"
-	x="Hellas"
+	const x  string = "Hello ,Bro" // here we assigned a constant as x in that we wote a string value.
+	//x="Hellas"    //if we assign any other string value in x it will give error -> cannot assign to x.
+	//becoz x is assigned as constant.
 	fmt.Println(x)
 }
