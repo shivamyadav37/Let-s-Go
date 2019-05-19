58b328cd53ec1877b6f08502a7f2272a16e5552c
Shivam Yadav <44369701+shivamyadav37@users.noreply.github.com>
2019-05-19 12:27:09 +0530

    Update variables.go

--- Programs/variables.go
+++ Programs/variables.go
@@ -1,6 +1,9 @@
+//Program tto depict use of Variables in Go Language..
+
 package main
 import "fmt"
 func main(){
-	var x string = "Heloo World"
-		fmt.Println(x)
+	var x string = "Heloo World"  // here we declare a variable x as a string variable 
+					// x is given a string value= "Heloo World"
+		fmt.Println(x)   //we call the variable x here
 }
